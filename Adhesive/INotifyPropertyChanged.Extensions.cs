1f7589d259117b4c06aa0d4628101a780f6e1f43
Dade Lamkins <dade@lamkins.org>
2019-03-11 18:01:42 -0400

    Began adding extension methods.

--- Adhesive/INotifyPropertyChanged.Extensions.cs
+++ Adhesive/INotifyPropertyChanged.Extensions.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using System.Text;
 
 namespace Adhesive {
     public static class INotifyPropertyChangedExtensions {
 
+        public static IReadOnlyCollection<Binding> GetBindings(this INotifyPropertyChanged instance) {
+            List<Binding> bindings = new List<Binding>();
 
+            //foreach (var member in BindManager.GetBindEndpoint(instance).CachedMembers.SelectMany<string, EndpointMember>(d => d.Value)) {
+                
+            //}
+
+            return new List<Binding>().AsReadOnly();
+        }
 
     }
 }
