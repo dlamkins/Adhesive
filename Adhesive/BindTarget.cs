35b02020738d1f2216f2d865a2a537d74a370b16
Dade Lamkins <dade@lamkins.org>
2019-03-18 20:28:01 -0400

    Changes for nuget package.

---
+++ Adhesive/BindTarget.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Adhesive {
+    public class BindTarget<TTargetMember> {
+
+        private object _targetInstance;
+
+        internal BindTarget(Expression<Func<TTargetMember>> bindTargetExpression) {
+            var targetReference = bindTargetExpression.Body as MemberExpression;
+
+            if (targetReference == null)
+                throw new ArgumentException($"{nameof(bindTargetExpression)} is expected to contain a MemberExpression, instead it got {bindTargetExpression.Body.NodeType}.");
+
+
+
+        }
+
+    }
+}
